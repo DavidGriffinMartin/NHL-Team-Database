fdba49fa822fdddde7a81dc3a97d1ec46c6500b6
David Griffin Martin <dgm@Davids-MacBook-Pro.local>
2022-05-09 10:54:36 -0400

    wip: Team data now translating. Added to HTML.

--- script.js
+++ script.js
@@ -7,10 +7,51 @@ const URL = 'https://statsapi.web.nhl.com/api/v1/teams'
 $.ajax(URL).then(function(data) {
     // Check work
     console.log('League info is ready!');
-    console.log(data.teams[0]);
-    let $result = data.teams[0];
-    $('.teams').append(`<li>${$result}</li>`)
+    console.log(data.teams[13]);
+    // 
+    const $wshName = data.teams[13].name;
+    const $wshAbv = data.teams[13].abbreviation;
+    const $wshConf = data.teams[13].conference.name;
+    const $wshDiv = data.teams[13].division.name;
+    const $wshVenue = data.teams[13].venue.name;
+    $('#team').append(`<li>Team name: ${$wshName}</li>`)
+    $('#team').append(`<li>Abbreviation: ${$wshAbv}</li>`)
+    $('#team').append(`<li>Conference: ${$wshConf}</li>`)
+    $('#team').append(`<li>Division: ${$wshDiv}</li>`)
+    $('#team').append(`<li>Venue: ${$wshVenue}</li>`)
+    // console.log(data.teams);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
