d484a752ccd60161b35802c4162859f8708f42ed
David Griffin Martin <dgm@Davids-MacBook-Pro.local>
2022-05-10 10:09:13 -0400

    wip: Added randomIndex to randomly Select a team. Updated styling to be more generic and changed header.

--- script.js
+++ script.js
@@ -1,94 +1,36 @@
 hello = 'Good morning, Dave.'
 console.log(hello)
 
-
 // NHL API
-const URL = 'https://statsapi.web.nhl.com/api/v1/teams'
+const URL = 'https://statsapi.web.nhl.com/api/v1/teams';
+const $getTeamBtn = $('#getTeamBtn');
+// const getTeam = () => {
+// 	const randomIndex = Math.floor(Math.random() * 32)
+// 	console.log(randomIndex)
+// 	const team = [randomIndex]
+// 	console.log(team)
+// }
 $.ajax(URL).then(function(data) {
     // CHECK API CONNECTION
     console.log('League info is ready!');
     console.log(data);
+    // DECLARE RANDOM INDEX
+    const randomIndex = Math.floor(Math.random() * 31)
+	console.log(randomIndex)
     // DECLARE VARIABLES
-    const $wshName = data.teams[13].name;
-    const $wshAbv = data.teams[13].abbreviation;
-    const $wshConf = data.teams[13].conference.name;
-    const $wshDiv = data.teams[13].division.name;
-    const $wshVenue = data.teams[13].venue.name;
-    const $wshEstb = data.teams[13].firstYearOfPlay
-    const $wshSite = data.teams[13].officialSiteUrl
+    const $teamName = data.teams[randomIndex].name;
+    const $teamAbv = data.teams[randomIndex].abbreviation;
+    const $teamConf = data.teams[randomIndex].conference.name;
+    const $teamDiv = data.teams[randomIndex].division.name;
+    const $teamVenue = data.teams[randomIndex].venue.name;
+    const $teamEstb = data.teams[randomIndex].firstYearOfPlay;
+    const $teamSite = data.teams[randomIndex].officialSiteUrl;
     // APPEND LIST ITEMS
-    $('#info').append(`<li>Team name: ${$wshName}</li>`)
-    $('#info').append(`<li>Abbreviation: ${$wshAbv}</li>`)
-    $('#info').append(`<li>Conference: ${$wshConf}</li>`)
-    $('#info').append(`<li>Division: ${$wshDiv}</li>`)
-    $('#info').append(`<li>Venue: ${$wshVenue}</li>`)
-    $('#info').append(`<li>Established: ${$wshEstb}</li>`)
-    $('#info').append(`<li><a href="${$wshSite}">Official Team Site</a></li>`)
-    // console.log(data.teams);
+    $('#info').append(`<li>Team name: ${$teamName}</li>`);
+    $('#info').append(`<li>Abbreviation: ${$teamAbv}</li>`);
+    $('#info').append(`<li>Conference: ${$teamConf}</li>`);
+    $('#info').append(`<li>Division: ${$teamDiv}</li>`);
+    $('#info').append(`<li>Venue: ${$teamVenue}</li>`);
+    $('#info').append(`<li>Established: ${$teamEstb}</li>`);
+    $('#info').append(`<li><a href="${$teamSite}">Official Team Site</a></li>`);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Guess how old you are by Name
-// const URL = 'https://api.agify.io/?name=Dave';
-
-// $.ajax(URL).then(function(data) {
-//     // Check work
-//     console.log('Space picture is ready!');
-//     console.log(data);
-// });
-
-
-// Kanye Quotes
-// const URL = 'https://api.kanye.rest';
-
-// $.ajax(URL).then(function(data) {
-//     // Check work
-//     console.log('Kanye Quote is ready!');
-//     console.log(data);
-// });
-
-
-// NASA Picture of the Day
-// const URL = 'https://images-api.nasa.gov/GET search?q=space';
-
-// $.ajax(URL).then(function(data) {
-//     // Check work
-//     console.log('Space picture is ready!');
-//     console.log(data);
-// });
