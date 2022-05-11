ea26c4c397a6a7d0f885957b8838b0562d3bba19
David Griffin Martin <dgm@Davids-MacBook-Pro.local>
2022-05-11 11:54:13 -0400

    wip: Added roster syncing.

--- script.js
+++ script.js
@@ -1,37 +1,54 @@
 hello = 'Good morning, Dave.'
-console.log(hello)
+console.log(hello);
 
 // NHL API (TEAMS)
-const URL = 'https://statsapi.web.nhl.com/api/v1/teams';
-// CACHE ELEMENTS
+const TEAMURL = 'https://statsapi.web.nhl.com/api/v1/teams?expand=team.roster';
 const $getTeamBtn = $('#getTeamBtn');
-const $infoContent = $('#info')
+const $infoContent = $('#info');
+const $namesContent = $('#names');
 // GET TEAM FUNCTION
 const getTeam = () => {
     $infoContent.empty();
-$.ajax(URL).then(function(data) {
-    // CHECK API CONNECTION
+    $namesContent.empty();
+$.ajax(TEAMURL).then(function(teamData) {
     console.log('League info is ready!');
-    console.log(data);
     // DECLARE RANDOM INDEX
-    const randomIndex = Math.floor(Math.random() * 31)
-	console.log(randomIndex)
-    // DECLARE VARIABLES
-    const $teamName = data.teams[randomIndex].name;
-    const $teamAbv = data.teams[randomIndex].abbreviation;
-    const $teamConf = data.teams[randomIndex].conference.name;
-    const $teamDiv = data.teams[randomIndex].division.name;
-    const $teamVenue = data.teams[randomIndex].venue.name;
-    const $teamEstb = data.teams[randomIndex].firstYearOfPlay;
-    const $teamSite = data.teams[randomIndex].officialSiteUrl;
+    const randomIndex = Math.floor(Math.random() * 31);
+	console.log('selected Team: ' + randomIndex);
+    // CHECK API CONNECTION
+    console.log(teamData);
+    // CACHE VARIABLES
+    const $teamName = teamData.teams[randomIndex].name;
+    const $teamAbv = teamData.teams[randomIndex].abbreviation;
+    const $teamConf = teamData.teams[randomIndex].conference.name;
+    const $teamDiv = teamData.teams[randomIndex].division.name;
+    const $teamVenue = teamData.teams[randomIndex].venue.name;
+    const $teamEstb = teamData.teams[randomIndex].firstYearOfPlay;
+    const $teamSite = teamData.teams[randomIndex].officialSiteUrl;
+    const $teamRoster = teamData.teams[randomIndex].roster.roster;
+    // APPEND INFO HEADER
+    $('#info').append(`<h3>Team Information</h3>`);
     // APPEND LIST ITEMS
-    $('#info').append(`<li>Team name: ${$teamName}</li>`);
+    $('#info').append(`<li>${$teamName}</li>`);
     $('#info').append(`<li>Abbreviation: ${$teamAbv}</li>`);
     $('#info').append(`<li>Conference: ${$teamConf}</li>`);
     $('#info').append(`<li>Division: ${$teamDiv}</li>`);
     $('#info').append(`<li>Venue: ${$teamVenue}</li>`);
     $('#info').append(`<li>Established: ${$teamEstb}</li>`);
     $('#info').append(`<li><a href="${$teamSite}">Official Team Site</a></li>`);
+    // APPEND ROSTER HEADING
+    $('#info').append(`<h3>Active Roster</h3>`);
+    // APPEND ROSTER LIST
+    $teamRoster.forEach((rosterData) => {
+        console.log(rosterData.person.fullName);
+        const $names = $('#names');
+        $names.text(rosterData.person.fullName);
+        $names.append($names);
+    })
+
+
+
+    console.log($teamRoster);
 })};
 // EVENT LISTINERS
 $getTeamBtn.on('click', getTeam)
